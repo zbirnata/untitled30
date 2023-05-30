dff9de4ef643c913fa1af0ded980d654b6bc744b
zbirnata <127501911+zbirnata@users.noreply.github.com>
2023-05-30 10:56:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        testClass.setNum1(2);
+        testClass.setNum2(3);
+        testClass.setNum3(4);
+
+
+    }
+}
